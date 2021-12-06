1adb3711197856d2ff57c1d86d63c13d2c5b949a
Toamig <carlosmiguelvieira@hotmail.com>
2021-12-06 01:32:15 +0000

    improve mirror camera behaviour
    use physical camera properties to update FOV in real time

--- Assets/Resources/Scripts/MirrorCamera.cs
+++ Assets/Resources/Scripts/MirrorCamera.cs
@@ -4,32 +4,58 @@ using UnityEngine;
 
 public class MirrorCamera : MonoBehaviour
 {
-    public Transform planeTransform;
+    [SerializeField] private Transform planeTransform;
 
     private GameObject mainCamera;
-    private string mainCameraTag = "MainCamera";
+    private readonly string mainCameraTag = "MainCamera";
 
     // Start is called before the first frame update
     void Start()
     {
         mainCamera = GameObject.FindGameObjectWithTag(mainCameraTag);
+        gameObject.GetComponent<Camera>().usePhysicalProperties = true;
     }
 
     // Update is called once per frame
     void Update()
     {
 
+        UpdatePosition();
+
+        UpdateProperties();
+
+        // Vectors to help visualize mirror camera positioning (Uncomment for debug purposes)
+        //Debug.DrawLine(mainCamera.transform.position, planeTransform.position, Color.green, 0, false);
+        //Debug.DrawLine(planeTransform.position, planeTransform.position + reflectionY, Color.red, 0, false);
+
+    }
+
+    // Updates camera position based on player's relative position to the mirror plane
+    public void UpdatePosition()
+    {
+      
         Vector3 reflectionX = Vector3.Reflect(planeTransform.position - mainCamera.transform.position, planeTransform.right);
 
         Vector3 reflectionY = Vector3.Reflect(planeTransform.position + reflectionX - planeTransform.position, planeTransform.forward);
 
         transform.position = planeTransform.position + reflectionY;
 
-        transform.LookAt(planeTransform.position);
+    }
 
-        // Vectors to help visualize mirror camera positioning (Uncomment for debug purposes)
-        //Debug.DrawLine(mainCamera.transform.position, planeTransform.position, Color.green, 0, false);
-        //Debug.DrawLine(planeTransform.position, planeTransform.position + reflectionY, Color.red, 0, false);
+    // Updates physical camera properties based on camera relative position to the mirror plane
+    public void UpdateProperties()
+    {
+        Renderer planeRenderer = planeTransform.gameObject.GetComponent<MeshRenderer>();
 
+        float planeSide = planeRenderer.bounds.size.x;
+
+        Vector3 direction = planeTransform.position - transform.position;
+
+        float shiftX = direction.x / planeSide;
+        float shiftY = direction.y / planeSide;
+
+        gameObject.GetComponent<Camera>().focalLength = Vector3.Distance(transform.position, planeTransform.position);
+        gameObject.GetComponent<Camera>().sensorSize = new Vector2(planeSide, planeSide);
+        gameObject.GetComponent<Camera>().lensShift = new Vector2(-shiftX, shiftY);
     }
 }
