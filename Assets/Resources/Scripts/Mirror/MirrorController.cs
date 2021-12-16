6401425f8bb930cab145fec594ff75aa4f89c63b
Toamig <carlosmiguelvieira@hotmail.com>
2021-12-16 05:43:53 +0000

    allow mirror movement through inputs

--- Assets/Resources/Scripts/Mirror/MirrorController.cs
+++ Assets/Resources/Scripts/Mirror/MirrorController.cs
@@ -6,6 +6,7 @@ public class MirrorController : MonoBehaviour
 {
 
     public GameObject mirror;
+    public GameObject player;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,8 @@ public class MirrorController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        // right hand controller (horizontal and vertical joystick axis)
         foreach (Transform child in mirror.transform)
         {
             child.RotateAround(mirror.transform.GetChild(0).position, mirror.transform.GetChild(0).transform.right, Input.GetAxis("Vertical") * 20 * Time.deltaTime);
@@ -26,9 +29,14 @@ public class MirrorController : MonoBehaviour
             child.RotateAround(mirror.transform.GetChild(0).position, mirror.transform.GetChild(0).transform.forward, - Input.GetAxis("Horizontal") * 20 * Time.deltaTime);
         }
 
+        // left hand controller (horizontal and vertical joystick axis)
         foreach (Transform child in mirror.transform)
         {
-            child.RotateAround(child.position, mirror.transform.GetChild(0).transform.up, Input.GetAxis("Rotate") * 20 * Time.deltaTime);
+            child.RotateAround(child.position, mirror.transform.GetChild(0).transform.up, Input.GetAxis("Horizontal2") * 20 * Time.deltaTime);
         }
+
+        Vector3 newPos = Vector3.MoveTowards(mirror.transform.GetChild(0).transform.position, player.transform.position, Input.GetAxis("Vertical2") * 20 * Time.deltaTime);
+        mirror.transform.GetChild(0).transform.position = newPos;
+        
     }
 }
