313b8d522861ae9ed52f6c7710adfb33b977c15e
Tomás Santos <75134545+tomassantos99@users.noreply.github.com>
2022-01-07 10:43:03 +0000

    Scene enrichment and simple stopwatch.

---
+++ Assets/Resources/Scripts/Stopwatch/Stopwatch.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Stopwatch : MonoBehaviour
+{
+
+    bool stopwatchActive = false;
+    float currentTime;
+    public Text currentTimeText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("return"))
+        {
+            stopwatchActive = false;
+            currentTime = 0;
+        }
+        if (Input.GetKeyDown("space"))
+        {
+            stopwatchActive = !stopwatchActive;
+        }
+        if (stopwatchActive)
+        {
+            currentTime = currentTime + Time.deltaTime;
+        }
+        TimeSpan time = TimeSpan.FromSeconds(currentTime);
+        currentTimeText.text = time.ToString(@"mm\:ss\:fff");
+    }
+
+
+}
